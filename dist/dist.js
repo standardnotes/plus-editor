21c5a36e2e489b917d9216700dc5b415bafd7624
Ningsong Shen <ningsongshen123@outlook.com>
2020-06-04 13:55:21 -0400

    fix: tab key inserts tab space

--- dist/dist.js
+++ dist/dist.js
@@ -797,6 +797,7 @@ document.addEventListener("DOMContentLoaded", function (event) {
       minHeight: null, // set minimum height of editor
       maxHeight: null, // set maximum height of editor
       focus: true, // set focus to editable area after initializing summernote
+      tabDisable: true, // set tab interaction to note only
       toolbar: [
       // [groupName, [list of button]]
       ['para', ['style']], ['style', ['bold', 'italic', 'underline', 'strikethrough', 'clear']], ['fontsize', ['fontsize', 'fontname']], ['color', ['color']], ['para', ['ul', 'ol', 'paragraph']], ['height', ['height']], ['insert', ['table', 'link', 'hr', 'picture', 'video']], ['misc', ['codeview', 'help']]],
